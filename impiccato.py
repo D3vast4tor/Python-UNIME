b9e238ade18c616b425fbb5e2bedc53e752fe91b
D3vast4tor <93880490+D3vast4tor@users.noreply.github.com>
2022-04-09 19:37:42 +0200

    Update impiccato.py

--- impiccato.py
+++ impiccato.py
@@ -13,36 +13,37 @@ class Hanged_man():
 
     def stampa_parola(self,parola,lettera,stampa):
         for i in range(len(parola)):
-            if parola[i] == lettera:
-                stampa = stampa + lettera
+            if parola[i] == lettera and i == 0:
+                stampa += lettera
+            elif i != 0 and parola[i] == lettera:
+                index = i
             else:
-                stampa = stampa + '_'
+                stampa += '_'
         print(stampa)
         return stampa
     
         
         
 def main():
-    parola = input("inserire la parola: ")
     tentativi = 0
     lettere_indovinate = 0
+    parola = ''
     choise = ''
     while choise != 'n':
+        parola = input("inserire la parola: ")
         while tentativi < 5 and lettere_indovinate < len(parola):
             lettera = input("inserire la lettera: ")
-            Hanged_man._found = Hanged_man.controllo_lettera(lettera,parola)
-            if Hanged_man._found == True:
+            if Hanged_man.controllo_lettera(Hanged_man,lettera,parola) == True:
                 lettere_indovinate += 1
+                Hanged_man._found = False
             else:
                 tentativi += 1
         if lettere_indovinate == len(parola):
-            print("Bravissimo, hai indovinato la parola che era ",parola)
+            print("Bravissimo, hai indovinato la parola che era: ",parola)
         else:
-            print("Sei troppo scarso, te l'avevo detto. La parola era ",parola)
+            print("Sei troppo scarso, te l'avevo detto. La parola era: ",parola)
         choise = input("Continuare?(s/n): ")
-        if choise == 'n':
-            print(colorama.Fore.GREEN + "Va bene, ritorna a giocare quando vuoi.")
-            quit(0)
+    print(colorama.Fore.GREEN + "Va bene, ritorna a giocare quando vuoi.")
         
 if __name__ == '__main__':
     main()
