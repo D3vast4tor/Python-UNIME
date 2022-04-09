f617570cbe9a0cfcbee22e3ed157171ceeddab0e
D3vast4tor <93880490+D3vast4tor@users.noreply.github.com>
2022-04-09 20:29:56 +0200

    Add files via upload

--- impiccato.py
+++ impiccato.py
@@ -1,47 +1,43 @@
-import colorama,time
+import colorama,time,pprint
 class Hanged_man():
-    _found = False
-    def controllo_lettera(self,lettera,parola):
-        if lettera in parola:
-            print(colorama.Fore.GREEN + "la lettera %c e presente nella parola" % lettera)
-            stampa = self.stampa_parola(parola,lettera,stampa)
-            self._found = True
-        else:
-            print(colorama.Fore.RED + "scarsoo, la lettera %c non e presente nella parola" % lettera)
-            trovato = 0
-        return trovato
-
-    def stampa_parola(self,parola,lettera,stampa):
-        for i in range(len(parola)):
-            if parola[i] == lettera and i == 0:
-                stampa += lettera
-            elif i != 0 and parola[i] == lettera:
-                index = i
-            else:
-                stampa += '_'
-        print(stampa)
-        return stampa
-    
-        
-        
-def main():
-    tentativi = 0
+    to_guess_word = ''
+    _guessed_word = dict()
+    _parola_trovata = ''
+    _tentativi = 0
     lettere_indovinate = 0
-    parola = ''
-    choise = ''
-    while choise != 'n':
-        parola = input("inserire la parola: ")
-        while tentativi < 5 and lettere_indovinate < len(parola):
+    def __init__(self,parola):
+        for i in range(len(parola)):
+            Hanged_man._guessed_word[i] = '_'     
+    def controllo_lettera(self,lettera,parola):
+        for i in range(len(self.to_guess_word)):
+            if lettera in parola:
+                self._guessed_word[i] = lettera
+                return True 
+    
+    def update_guess(self):
+        while self._tentativi < 4 and self.lettere_indovinate < len(self.to_guess_word):
             lettera = input("inserire la lettera: ")
-            if Hanged_man.controllo_lettera(Hanged_man,lettera,parola) == True:
-                lettere_indovinate += 1
+            self._parola_trovata = ''
+            if Hanged_man.controllo_lettera(Hanged_man,lettera,self.to_guess_word) == True:
+                for i in range(len(self.to_guess_word)):
+                    self._parola_trovata += self._guessed_word[i]
+                print( "\nLa parola da indovinare è: %s" % (self._guessed_word[i]) )
+                self.lettere_indovinate += 1
                 Hanged_man._found = False
             else:
-                tentativi += 1
-        if lettere_indovinate == len(parola):
-            print("Bravissimo, hai indovinato la parola che era: ",parola)
+                self._tentativi += 1
+        
+    
+def main():
+    choise = ''
+    while choise != 'n':
+        Hanged_man.to_guess_word = input("inserire la parola: ")
+        Hanged_man.__init__(Hanged_man,Hanged_man.to_guess_word)
+        Hanged_man.update_guess(Hanged_man)
+        if Hanged_man.lettere_indovinate == len(Hanged_man.to_guess_word):
+            print("Bravissimo, hai indovinato la parola che era: ",Hanged_man.to_guess_word)
         else:
-            print("Sei troppo scarso, te l'avevo detto. La parola era: ",parola)
+            print("Sei troppo scarso, te l'avevo detto. La parola era: ",Hanged_man.to_guess_word)
         choise = input("Continuare?(s/n): ")
     print(colorama.Fore.GREEN + "Va bene, ritorna a giocare quando vuoi.")
         
